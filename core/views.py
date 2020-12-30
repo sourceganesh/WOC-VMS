88c5c7c9608b97f02ef47008f773febb370bb7d5
ganesh <soccerganesh@gmail.com>
2020-12-30 21:07:51 +0530

    improvised save_image function

--- core/views.py
+++ core/views.py
@@ -4,9 +4,7 @@ from core.models import VehicleLogs
 from core.serializers import VehicleLogsSerializer
 from rest_framework import authentication, permissions
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
-from vehiclemanagement import settings
-
+from date
 
 # Create your views here.
 def index(request):
@@ -25,16 +23,21 @@ def webcam_index(request):
 
 @csrf_exempt
 def save_image(request):
-	# now = dt.dt.now
-	# if request.user.is_authenticated and request.user.is_admin:
-	if request.method == 'POST':
-		# save it somewhere
-		f = open(settings.MEDIA_ROOT + '/vehicle_image/now_request.user.admin_gate_id.jpg', 'wb')
-		f.write(request.body)
-		f.close()
-		# vehicle_number = modal.predict(string)
+	from django.http import HttpResponse
+	from vehiclemanagement import settings
+	from datetime import datetime as dt
+	if request.user.is_authenticated and request.user.is_admin:
+		if request.method == 'POST':
+			now = dt.now
+			image_name = str(now) + str(request.user.admin_gate_id)
+			# save it somewhere
+			f = open(settings.MEDIA_ROOT + f'/vehicle_image/{image_name}.jpg', 'wb')
+			f.write(request.body)
+			f.close()
+			vehicle_number = "RandomString"
+			# vehicle_number = modal.predict(string)
 
-		# return the URL
-		return HttpResponse('')
-	else:
-		return HttpResponse('no data')
+			# return the URL
+			return HttpResponse(f'The Vehicle Number has been Predicted as : {vehicle_number}')
+		else:
+			return HttpResponse('no data')
