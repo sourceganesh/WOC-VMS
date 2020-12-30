174762e1b5727cca6f0831a08b36c13723a54e16
ganesh <soccerganesh@gmail.com>
2020-12-30 20:04:51 +0530

    video feature v1 added stable

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,7 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name="index"),
     path('create', LogVehicle.as_view()),
-    path('api-auth/', include('rest_framework.urls', namespace='rest-framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest-framework')),
+    path('webcam/', views.webcam_index, name="webcam"),
+    path('webcam/add-image', views.save_image, name="webcam-add")
 ]
